f40a6d0b395ab8e8ccec6a98f6c41f3e9871d506
Maadeha shaikh <maadeham@gmail.com>
2024-08-16 22:56:48 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+
+// requiring module here 
+const userModel = require('./usermodel')
+
+app.get('/',(req,res)=>{
+ res.send("Hello Shaikh");
+})
+
+app.get('/create',async (req,res)=>{
+  let createdUser = await userModel.create({              // 
+    name:"maryam",              //       this all code is asynchronous code 
+    username:"maryam shaikh",  //
+    email:"maryam@gmail.com" //   
+  })                          //
+  res.send(createdUser);     //           this is synchronous code 
+  // and we must know that synchronous code pehly chlta h asynchronous bad mai 
+  // monhoDB say related mongoose kay through karnyt wala koi bh code jo hoga woh asynchronous he hoga so mongoose ka future mai jo bh code hoga na woh sab asynchronous hoga he hoga bcz its asynchronous we can use async await  
+  // 3 bytes its a time stamp machine ki info network info etc id mai hoti h  
+ }) 
+
+app.get('/update',async (req,res)=>{ 
+  let updatedUser = await userModel.findOneAndUpdate({name:"maadeha"},{name:"maryam"},{new:true});
+  res.send(updatedUser);
+  })
+
+app.get('/read', async (req,res) => { 
+      let users = await userModel.find(); // find say sab aa jata h array dyta h findone obj dayta h or pehla banda search kar kay dyta h 
+      res.send(users);
+  })  
+  
+  app.get('/delete', async (req,res) => { 
+    let users = await userModel.findOneAndDelete({name:"maadeha"});
+    res.send(users);
+  })  
+
+app.listen(3000);
---
+++ usermodel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+mongoose.connect(`mongodb://127.0.0.1:27017/firstDB`);
+ //it's a method jo kay accept karta h aik object ko 
+const userSchema = mongoose.Schema({    // schema matlb h kay har user kay pass kia kia details hongi 
+  name:String,
+  username:String,
+  email:String
+})
+//model kia karta h? asal mai woh collections bnata h & model ki base p he hm CRUD opperations ko kar skty hain 
+module.exports = mongoose.model("user",userSchema) ;
+//jo bh hm name dyty hain uska pluralized version bna jata h jaisy user => users
+// hmy yeh bh btana hota h kay schma konsa use karna h so we use UserSchema here 
+// agar hm kisi or route p kam karna chahty hain so that hm is module ko export kiyay baghair use nh kar skty 
+// exports is a propert and we export model 
